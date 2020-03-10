5e6d9bd82e30a83f6eb6ea02834cdc11a6f17a31
costa-daniele <santoscostadaniele@gmail.com>
2020-03-10 11:50:43 -0300

    adicionando arquivos em C

---
+++ 002 - comentarios/comentarios.c
@@ -0,0 +1,10 @@
+/*Parte Comentada do Olá Mundo!*/
+
+#include <stdio.h> //standard input output
+#include <stdlib.h> //biblioteca auxiliar
+
+//instrução
+int main(){
+    printf("Olá Mundo\nComentado\n");//função
+    return 0;
+}
